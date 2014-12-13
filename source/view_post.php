e6f1dee0364d0a256ab7287400cf97db6ea3df10
salih1f1 <salih1f1@hotmail.com>
2014-12-13 22:21:54 +0200

    view_post.php mod

--- source/view_post.php
+++ source/view_post.php
@@ -1,11 +1,32 @@
 <?php
 include 'config.php';
-include 'functions.php';
-if (isset($_GET['id'])) {
+if (isset($_GET['id'])):
+    $id = $_GET['id'];
     $stmt = $db->prepare("SELECT * FROM posts WHERE post_id = :id");
     $stmt->bindParam('id', $id, PDO::PARAM_STR);
     $stmt->execute();
-    $row = $stmt->fetchAll();
-    var_dump($row);
-}
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $row = $row[0];
+
+    $users = $db->prepare("SELECT * FROM users  WHERE user_id = :uid");
+    $users->bindParam('uid', $row['post_author'], PDO::PARAM_STR);
+    $users->execute();
+    $user = $users->fetchAll();
+    $user = $user[0][1];
+?>
+<!DOCTYPE>
+<html>
+<head>
+    <title><?=$row['post_title']?></title>
+</head>
+<body>
+    <h1><?=$row['post_title']?></h1>
+    <h2><?=$row['post_description']?></h2>
+    <div><?=$row['post_content']?></div>
+    <div>Published on <?=date('d/m/y') ?></div>
+    <div>Author: <?=$user?></div>
+</body>
+</html>
+<?php
+    endif;
 ?>
