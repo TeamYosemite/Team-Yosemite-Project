23764d45246b3bc52ea3d2d00265482016b8a1ba
Иван Захариев <izahariev96@gmail.com>
2014-12-14 20:14:48 +0200

    changed date format function

--- source/functions.php
+++ source/functions.php
@@ -3,12 +3,13 @@
 function load_posts($db) {
 	$stmt = $db->query('SELECT `post_id`, `post_title`, `post_description`, `post_author`, `post_dateCreated`, `post_timesSeen` FROM posts');
 	while($row = $stmt->fetch()) {
+	$dateCreated = new DateTime($row['post_dateCreated']);
 	?>
 		<article>
 			<h3><a href="view_post.php?id=<?= $row['post_id'];?>"><?= $row['post_title'];?></a></h3>
 			<p><?= $row['post_description'];?>'</p>
 			<p><a href="view_post.php?id=<?= $row['post_id'];?>">Read more</a></p>
-			<p>Posted on <?= date('d-m-Y H:i:s', $row['post_dateCreated']);?></p>
+			<p>Posted on <?= $dateCreated->format('d-m-Y H:i:s');?></p>
 		</article>
 	<?php
 	}
