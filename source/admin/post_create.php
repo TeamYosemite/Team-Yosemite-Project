85b67b4bb402062e9abbc31036dad54f8c4611fb
salih1f1 <salih1f1@hotmail.com>
2014-12-14 23:01:12 +0200

    n

--- source/admin/post_create.php
+++ source/admin/post_create.php
@@ -1,6 +1,22 @@
 <form method="POST">
 	<input type="text" name="title" placeholder="Title..." />
-	<textarea name="Description" placeholder="Description..." ></textarea>
-	<textarea name="Content" placeholder="Content..." ></textarea>
-	<input type="submit" value="Submit" />
+	<textarea name="description" placeholder="Description..." ></textarea>
+	<textarea name="content" placeholder="Content..." ></textarea>
+	<input type="submit" value="Submit" name="submit"/>
 </form>
+
+<?php
+include '../config.php';
+if (isset($_POST['submit'])) {
+    echo '76765';
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $content = $_POST['content'];
+    $user = 'me'; // Session
+    $sql = "INSERT INTO posts (post_title, post_description, post_content, post_author) VALUES ('$title', '$description', '$content', '$user')";
+    try {
+        $db->exec($sql);
+    } catch (Exception $e) {
+        echo $e;
+    }
+}
