c0f1ccb2ac7d32e4f3a5d2257c125e54a2724197
salih1f1 <salih1f1@hotmail.com>
2014-12-14 20:15:50 +0200

    ккк

--- source/functions.php
+++ source/functions.php
@@ -1,14 +1,13 @@
 <?php
-
 function load_posts($db) {
 	$stmt = $db->query('SELECT `post_id`, `post_title`, `post_description`, `post_author`, `post_dateCreated`, `post_timesSeen` FROM posts');
 	while($row = $stmt->fetch()) {
 	?>
 		<article>
-			<h3><a href="view_post.php?id=<?= $row['post_id'];?>"><?= $row['post_title'];?></a></h3>
-			<p><?= $row['post_description'];?>'</p>
-			<p><a href="view_post.php?id=<?= $row['post_id'];?>">Read more</a></p>
-			<p>Posted on <?= date('d-m-Y H:i:s', $row['post_dateCreated']);?></p>
+			<h3><a href="view_post.php?id=<?= $row['post_id'];?>"><?= $row['post_title']?></a></h3>
+			<p><?= $row['post_description']?>'</p>
+			<p><a href="view_post.php?id=<?=$row['post_id']?>">Read more</a></p>
+			<p>Posted on <?=date('d-m-Y H:i:s', $row['post_dateCreated'])?></p>
 		</article>
 	<?php
 	}
